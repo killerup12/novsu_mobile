87547b1bd0e337240fc53daa084f3af0f13e0b4e
grigorijbogdanov <ctr_grigorii.bogdanov@dotgovsolutions.net>
2022-12-12 23:21:00 +0300

    Added null checker for hive

--- lib/data/data_management/memory_access_provider_impl.dart
+++ lib/data/data_management/memory_access_provider_impl.dart
@@ -1,4 +1,6 @@
+import 'package:cookie_jar/cookie_jar.dart';
 import 'package:hive/hive.dart';
+import 'package:novsu_mobile/domain/exceptions/exceptions.dart';
 import 'package:novsu_mobile/domain/models/timetable.dart';
 import 'package:novsu_mobile/domain/models/user.dart';
 import 'package:novsu_mobile/domain/utils/memory_access_provider.dart';
@@ -10,7 +12,7 @@ class MemoryAccessProviderImpl extends MemoryAccessProvider {
 
   @override
   User getUser() {
-    return Hive.box('studentData').get(_user);
+    return _returnData(Hive.box('studentData').get(_user));
   }
 
   @override
@@ -20,11 +22,19 @@ class MemoryAccessProviderImpl extends MemoryAccessProvider {
 
   @override
   Timetable getTimetable() {
-    return Hive.box(boxName).get(_timetable);
+    return _returnData(Hive.box(boxName).get(_timetable));
   }
 
   @override
   void setTimetable(Timetable timetable) {
     Hive.box(boxName).put(_timetable, timetable);
+  }
+
+  _returnData(data) {
+    if (data != null) {
+      return data;
+    } else {
+      throw NotFoundInDeviceMemoryException();
+    }
   }
 }
