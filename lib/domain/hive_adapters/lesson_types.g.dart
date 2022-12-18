0e8e10f141160afc4c7c76a8b8de4c390e7277a9
grigorijbogdanov <ctr_grigorii.bogdanov@dotgovsolutions.net>
2022-12-18 17:58:43 +0300

    Reworked theme and minor changes

---
+++ lib/domain/hive_adapters/lesson_types.g.dart
@@ -0,0 +1,51 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of '../enums/lesson_types.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class LessonTypesAdapter extends TypeAdapter<LessonTypes> {
+  @override
+  final int typeId = 4;
+
+  @override
+  LessonTypes read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return LessonTypes.lecture;
+      case 1:
+        return LessonTypes.practice;
+      case 2:
+        return LessonTypes.unknown;
+      default:
+        return LessonTypes.lecture;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, LessonTypes obj) {
+    switch (obj) {
+      case LessonTypes.lecture:
+        writer.writeByte(0);
+        break;
+      case LessonTypes.practice:
+        writer.writeByte(1);
+        break;
+      case LessonTypes.unknown:
+        writer.writeByte(2);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is LessonTypesAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
