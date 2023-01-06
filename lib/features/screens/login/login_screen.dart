d21941df9758ea1e5ee47e0d39abea3c9ad2bce5
grigorijbogdanov <ctr_grigorii.bogdanov@dotgovsolutions.net>
2023-01-06 04:00:49 +0300

    added focus change for login screen. Username field redirects on password field. Password field closes keyboard and tries to login

--- lib/features/screens/login/login_screen.dart
+++ lib/features/screens/login/login_screen.dart
@@ -69,6 +69,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       decoration: const InputDecoration(
                         hintText: 'login',  //TODO i18n
                       ),
+                      textInputAction: TextInputAction.next,
                     ),
                   ),
                   Container(
@@ -85,6 +86,11 @@ class _LoginScreenState extends State<LoginScreen> {
                       readOnly: checkIsWaitingResponse(state),
                       decoration: const InputDecoration(
                         hintText: 'password', //TODO i18n
+                      ),
+                      textInputAction: TextInputAction.done,
+                      onEditingComplete: () => tryToLogIn(
+                          loginTextController.text,
+                          passwordTextController.text
                       ),
                     ),
                   ),
